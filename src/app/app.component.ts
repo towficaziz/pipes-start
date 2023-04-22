f967d7b28a61ab880882fcb4e7cb4f1a17dada50
towficaziz <tapolly01@gmail.com>
2023-04-22 15:29:24 +0200

    Example: Creating a filter Pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,6 +32,7 @@ export class AppComponent {
       started: new Date(15, 1, 2017)
     }
   ];
+  filteredStatus ='';
   getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
     return {
       'list-group-item-success': server.status === 'stable',
@@ -39,4 +40,12 @@ export class AppComponent {
       'list-group-item-danger': server.status === 'critical'
     };
   }
+  // onAddServer(){
+  //   this.servers.push({
+  //     instanceType: 'small',
+  //     name: 'New server',
+  //     status: 'stable',
+  //     started: new Date(15, 1, 2023)
+  //   });
+  // }
 }
